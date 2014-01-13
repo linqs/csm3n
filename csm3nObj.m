3c6ffba93166b1246370dfb03538a67c3a81bf52
Ben London <blondon@cs.umd.edu>
2014-01-13 11:43:16 -0500

    more edits to CSM3N objective, but still broken.

--- csm3nObj.m
+++ csm3nObj.m
@@ -29,25 +29,43 @@ function [stab, sg] = stabilityObj(w, ex, decodeFunc, varargin)
 	[nodePot,edgePot] = UGM_CRF_makePotentials(w,ex.Xnode,ex.Xedge,ex.nodeMap,ex.edgeMap,ex.edgeStruct);
 	y_unp = decodeFunc(nodePot,edgePot,ex.edgeStruct,varargin{:});
 
+	% init worst perturbation to current X, and y_per = y_unp
+	Xnode = ex.Xnode;
+	Xedge = ex.Xedge;
+	y_per = y_unp;
+	
+	% create constraints on perturbation
+	
 	% find worst perturbation
-	Xnode = zeros(1,ex.nFeat,ex.nNode);
-	Xedge = zeros(ex.nState,ex.nState,ex.nEdge,ex.nFeat);
+	
+	% NOT NEEDED IF OPTIMIZATION OUTPUTS STABILITY
+% 	% loss-augmented inference for perturbed input
+% 	Ynode = overcompleteRep(y_unp,ex.nState,0);
+% 	y_per = lossAugInfer(w,Xnode,Xedge,Ynode,ex.nodeMap,ex.edgeMap,ex.edgeStruct,decodeFunc,varargin{:});
+% 	
+% 	% L1 distance between predictions
+% 	stab = norm(y_unp-y_per, 1);
+	
+function [f, g] = perturbObj(x, w, Ynode_unp, nodeMap, edgeMap, edgeStruct, lag, decodeFunc, varargin)
+	
+	% reconstruct Xnode, Xedge from x
+	[nNode,nState,nFeat] = size(nodeMap);
+	Xnode = reshape(x, nFeat, nNode);
+	Xedge = UGM_makeEdgeFeatures(Xnode,edgeStruct.edgeEnds);
 	
 	% loss-augmented inference for perturbed input
-	Ynode = overcompleteRep(y_unp,ex.nState,0);
-	y_per = lossAugInfer(w,Xnode,Xedge,Ynode,ex.nodeMap,ex.edgeMap,ex.edgeStruct,decodeFunc,varargin{:});
-	
+	y_per = lossAugInfer(w,Xnode,Xedge,Ynode_unp,nodeMap,edgeMap,edgeStruct,decodeFunc,varargin{:});
+	Ynode_per = overcompleteRep(y_per,ex.nState,0);
+
 	% L1 distance between predictions
-	stab = norm(y_unp-y_per, 1);
+	stab = norm(Ynode_unp - Ynode_per, 1);
+
+	% NOT NEEDED?
+% 	% compute objective and gradient
+% 	[f1, g1] = Xobj(Wnode,Wedge,Xnode,Ynode_unp,Yedge{1});
+% 	[f2, g2] = Xobj(Wnode,Wedge,Xnode,Ynode_per,Yedge{2});
+% 	f = lag * (f1 + f2);
+% 	g = lag * (g1 + g2);
 	
-function [f, g] = perturbObj(x, Wnode, Wedge, Ynode, Yedge, lag)
 	
-	% reconstruct Xnode from x
-	Xnode = zeros(nFeat,nNode);
-	
-	% compute objective and gradient for Y1,Y2
-	[f1, g1] = Xobj(Wnode,Wedge,Xnode,Ynode{1},Yedge{1});
-	[f2, g2] = Xobj(Wnode,Wedge,Xnode,Ynode{2},Yedge{2});
-	f = lag * (f1 + f2);
-	g = lag * (g1 + g2);
 	
