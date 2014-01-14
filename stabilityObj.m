d601a906a3cef9fdd6b7284dcd55e899dde65b7e
Ben London <blondon@cs.umd.edu>
2014-01-14 10:46:38 -0500

    fixed bug in stabilityObj. might be working now, but can't satisfy ineq constraints

--- stabilityObj.m
+++ stabilityObj.m
@@ -17,19 +17,22 @@ x0 = reshape(squeeze(Xnode(1,:,:)),[],1);
 % create constraints on perturbation
 A = ones(size(x0')) - 2*x0';
 b = 1 - sum(x0);
-lb = zeros(size(length(x0)));
-ub = ones(size(length(x0)));
+lb = zeros(size(x0));
+ub = ones(size(x0));
 
 % perturbation objective
 obj = @(x,varargin) perturbObj(x,w,yoc_unp,nodeMap,edgeMap,edgeStruct,decodeFunc,varargin{:});
 
 % find worst perturbation as LP
-options = optimset('GradObj','on','Display','iter','MaxIter',10);
+options = optimset('GradObj','on','Algorithm','interior-point' ...
+				  ,'Display','iter','MaxIter',100,'TolFun',1);
 [x_per,f,~,~,~,g] = fmincon(obj,x0,A,b,[],[],lb,ub,[],options);
+f = -f;
+g = -g;
 
 
 function [f, g] = perturbObj(x, w, yoc_unp, nodeMap, edgeMap, edgeStruct, decodeFunc, varargin)
-	
+
 	% reconstruct Xnode, Xedge from x
 	[nNode,nState,nFeat] = size(nodeMap);
 	edgeEnds = edgeStruct.edgeEnds;
@@ -48,15 +51,17 @@ function [f, g] = perturbObj(x, w, yoc_unp, nodeMap, edgeMap, edgeStruct, decode
 	Xnode = squeeze(Xnode); % first dimension only has 1 entry
 	f = 0;
 	g = zeros(size(Xnode));
+	Wnode = squeeze(w(nodeMap(1,:,:)))';
 	for i = 1:nNode
-		Wnode = squeeze(w(nodeMap(i,:,:)))';
+		%Wnode = squeeze(w(nodeMap(i,:,:)))';
 		idx = localIndex(i,1:nState,nState);
 		dy = yoc_per(idx) - yoc_unp(idx);
 		f = f + sum(sum( Wnode .* (Xnode(:,i) * dy') ));
 		g(:,i) = Wnode * dy;
 	end
+	Wedge = w(reshape(squeeze(edgeMap(:,:,1,:)),nState^2,2*nFeat)');
 	for e = 1:size(edgeEnds,1)
-		Wedge = w(reshape(squeeze(edgeMap(:,:,e,:)),nState^2,2*nFeat)');
+		%Wedge = w(reshape(squeeze(edgeMap(:,:,e,:)),nState^2,2*nFeat)');
 		i = edgeEnds(e,1);
 		j = edgeEnds(e,2);
 		idx = pairwiseIndex(e,1:nState,1:nState,nNode,nState);
