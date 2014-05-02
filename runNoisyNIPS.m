c906e4e4a67176bbb4dd10a9a9d25447ad1a2349
Ben London <blondon@cs.umd.edu>
2014-05-02 15:48:35 -0400

    changed noisy nips

--- runNoisyNIPS.m
+++ runNoisyNIPS.m
@@ -6,7 +6,8 @@
 %   runAlgos (def: [2 4 5])
 %   inferFunc (def: UGM_Infer_TRBP)
 %   decodeFunc (def: UGM_Decode_TRBP)
-%   filename (def: will not save)
+%   save2file (def: will not save)
+%	makePlots (def: 0)
 
 if ~exist('nFold','var')
 	nFold = 10;
@@ -23,12 +24,25 @@ end
 if ~exist('decodeFunc','var')
 	decodeFunc = @UGM_Decode_TRBP;
 end
+if ~exist('makePlots','var')
+	makePlots = 0;
+end
+
+if makePlots
+	dataFig = 101;
+	objFig = 102;
+	predFig = 103;
+else
+	dataFig = 0;
+	objFig = 0;
+	predFig = 0;
+end
 
 cd data/nips14;
 nTrain = 1;
 nCV = 1;
 nTest = 10;
-[examples] = loadExamples(nFold*(nTrain+nCV+nTest),noiseRate,.5,1,1,101);
+[examples] = loadExamples(nFold*(nTrain+nCV+nTest),noiseRate,.6,1,1,dataFig);
 for f = 1:nFold
 	sidx = (f-1)*(nTrain+nTest);
 	foldIdx(f).tridx = sidx+1:sidx+nTrain;
@@ -42,14 +56,17 @@ expSetup = struct('foldIdx',foldIdx ...
 				 ,'runAlgos',[2 4 5] ...
 				 ,'decodeFunc',@UGM_Decode_TRBP,'inferFunc',@UGM_Infer_TRBP ...
 				 ,'Cvec',[.001 .01 .1 1 10 100 1000] ...
-				 ,'stepSizeVec',[.02 .04 .06] ...
+				 ,'stepSizeVec',[.01 .02 .05] ...
 				 ,'kappaVec',[.1 .2 .5 1 2 5 10 20 50 100] ...
 				 );
 expSetup.optSGD = struct('maxIter',1000 ...
-						,'plotObj',102,'plotRefresh',100 ...
+						,'plotObj',objFig,'plotRefresh',100 ...
 						,'verbose',0,'returnBest',1);
 expSetup.optLBFGS = struct('Display','iter');
-expSetup.plotPred = 103;
-experiment
+expSetup.plotPred = predFig;
+
+if exist('save2file','var')
+	expSetup.save2file = save2file;
+end
 
 experiment;
