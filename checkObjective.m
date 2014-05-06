7c5200f1950f45fd24e2aabd287681c7587d2fd6
Bert Huang <bert@cs.umd.edu>
2014-05-06 17:36:32 -0400

    added objective check function

---
+++ checkObjective.m
@@ -0,0 +1,24 @@
+function [] = checkObjective(func, x, epsilon)
+
+d = length(x);
+dir = rand(d,1);
+dir = dir / norm(dir);
+
+increments = linspace(-epsilon,epsilon,50);
+
+y = zeros(size(increments));
+
+for i = 1:length(increments)
+    y(i) = func(x + increments(i) * dir);
+end
+
+
+plot(increments, y);
+
+xlabel('distance from current point');
+ylabel('objective value');
+
+hold on;
+plot(0, func(x), 'x');
+hold off;
+
